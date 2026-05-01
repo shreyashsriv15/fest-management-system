f5b5bb588fbcbca52019b73e1db9493bda0fafaf
vibhavmathur2108-prog <vibhavmathur2108@gmail.com>
2026-05-02 01:59:39 +0530

    Added login page initials in main function

--- project.cpp
+++ project.cpp
@@ -1,2 +1,28 @@
 #include <iostream>
+#include "Admin.h"
 using namespace std;
+
+int main()
+{
+    Admin a1;
+
+    int n;
+
+    cout << "New Admin ? Press 1 to create an account and press 2 to login" << endl;
+    cin >> n;
+
+    if(n == 1)
+    {
+        a1.createAccount();
+
+        string user, pass;
+
+        cout<<"Enter the username again : "<<endl;
+        cin>>user;
+
+        cout<<"Enter the password again : "<<endl;
+        cin>>pass;
+
+        a1.login(user,pass);
+    }
+}
