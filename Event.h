fe361b41f87dbcf4dc52b326515cd55c352cc040
vibhavmathur2108-prog <vibhavmathur2108@gmail.com>
2026-05-04 14:21:08 +0530

    Update Event.h

--- Event.h
+++ Event.h
@@ -1,31 +1,36 @@
 #ifndef EVENT_H
 #define EVENT_H
 
+#include <string>
+using namespace std;
+
 class Event
 {
-    private:
-        string eventName[50];
-        string organiser[50];
-        string date[50];
-        string time[50];
-        string venue[50];
-        string winner[50];
-        
-        int count;
+protected:
+    string eventName[50];
+    string organiser[50];
+    string date[50];
+    string time[50];
+    string venue[50];
+    string winner[50];
 
-    public:
-        //Constructor
-        Event();
+    int count;
 
-        //Member Functions
-        void addEvent();
-        void deleteEvent();
-        void addSchedule();
-        void viewEvents();
-        void declareWinner();
-        void generateReport();
-        void searchEvent();
-        void organiserDetails();
+public:
+    Event();
+
+    //Virtual functions (Polymorphism)
+    virtual void addEvent();
+    virtual void deleteEvent();
+    virtual void viewEvents();
+    virtual void generateReport();
+
+    void addSchedule();
+    void declareWinner();
+    void searchEvent();
+    void organiserDetails();
+
+    virtual ~Event() {}
 };
 
 #endif
