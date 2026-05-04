fd15cdb91d6369a4f39014dcb4b8b564ba60d7cc
vibhavmathur2108-prog <vibhavmathur2108@gmail.com>
2026-05-04 14:23:33 +0530

    Update participants.h

--- participants.h
+++ participants.h
@@ -1,20 +1,28 @@
 #ifndef PARTICIPANTS_H
 #define PARTICIPANTS_H
+
 #include <string>
+#include "Event.h"
 using namespace std;
+
 class Participants : public Event
 {
-    private :
+private:
     string participantName[100];
     string eventforParticipant[100];
-    int count = 0;
-    int maxParticipants=0;
-    public :
-    // Add max participants for event  
+    int pCount = 0;
+    int maxParticipants = 0;
+
+public:
     void setMaxParticipants();
     void addParticipant();
     void deleteParticipant();
     void viewParticipants();
     void shortlistParticipants();
+
+    //Overriding 
+    void viewEvents() override;
+    void generateReport() override;
 };
+
 #endif
