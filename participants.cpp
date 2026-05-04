2955a65b72f78f3d80956f510ea351958e577339
vibhavmathur2108-prog <vibhavmathur2108@gmail.com>
2026-05-04 14:24:26 +0530

    Update participants.cpp

--- participants.cpp
+++ participants.cpp
@@ -1,86 +1,90 @@
 #include <iostream>
-#include <string>
-#include "Event.h"
 #include "participants.h"
-using namespace std ;
-    // Add max participants for event  
-    void Participants :: setMaxParticipants()
+using namespace std;
+
+void Participants::setMaxParticipants()
+{
+    cout<<"Enter Maximum Participants Allowed: ";
+    cin>>maxParticipants;
+}
+
+void Participants::addParticipant()
+{
+    if(pCount >= maxParticipants)
     {
-        cout<<"Enter Maximum Participants Allowed : "<<endl;
-        cin>>maxParticipants;
-    }
-    void Participants :: addParticipant()
-    {
-        if(count >= maxParticipants)
-        {
-            cout<<"Maximum participant limit reached\n";
-            return;
-        }
-
-        cout<<"Enter Participant Name: ";
-        cin>>participantName[count];
-
-        cout<<"Enter Event Name: ";
-        cin>>eventforParticipant[count];
-
-        count++;
-    }
-    void Participants :: deleteParticipant()
-    {
-        string name;
-
-        cout<<"Enter Participant Name to Delete: ";
-        cin>>name;
-
-        for(int i=0;i<count;i++)
-        {
-            if(participantName[i] == name)
-            {
-                for(int j=i;j<count-1;j++)
-                {
-                    participantName[j] = participantName[j+1];
-                    eventforParticipant[j] = eventforParticipant[j+1];
-                }
-
-                count--;
-
-                cout<<"Participant Removed\n";
-                return;
-            }
-        }
-    }
- 
-     void Participants :: viewParticipants()
-   {
-    if(count == 0)
-    {
-        cout<<"No participants added yet\n";
+        cout<<"Limit reached\n";
         return;
     }
 
-    for(int i=0;i<count;i++)
-    {
-        cout<<"\nParticipant Name : "<<participantName[i];
-        cout<<"\nEvent Name       : "<<eventforParticipant[i]<<endl;
-    }
-   }
-    void Participants :: shortlistParticipants()
-    {
-        int n;
+    cout<<"Enter Participant Name: ";
+    cin>>participantName[pCount];
 
-        cout<<"Enter Number of Participants to Shortlist: ";
-        cin>>n;
+    cout<<"Enter Event Name: ";
+    cin>>eventforParticipant[pCount];
 
-        if(n > count)
+    pCount++;
+}
+
+void Participants::deleteParticipant()
+{
+    string name;
+    cout<<"Enter Participant Name: ";
+    cin>>name;
+
+    for(int i=0;i<pCount;i++)
+    {
+        if(participantName[i] == name)
         {
-            cout<<"Not enough participants\n";
+            for(int j=i;j<pCount-1;j++)
+            {
+                participantName[j] = participantName[j+1];
+                eventforParticipant[j] = eventforParticipant[j+1];
+            }
+            pCount--;
+            cout<<"Removed\n";
             return;
         }
-
-        cout<<"\nShortlisted Participants:\n";
-
-        for(int i=0;i<n;i++)
-        {
-            cout<<participantName[i]<<endl;
-        }
     }
+}
+
+void Participants::viewParticipants()
+{
+    if(pCount == 0)
+    {
+        cout<<"No participants\n";
+        return;
+    }
+
+    for(int i=0;i<pCount;i++)
+    {
+        cout<<"\n"<<participantName[i]<<" -> "<<eventforParticipant[i];
+    }
+}
+
+void Participants::shortlistParticipants()
+{
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+
+    for(int i=0;i<n && i<pCount;i++)
+    {
+        cout<<participantName[i]<<endl;
+    }
+}
+
+void Participants::viewEvents()
+{
+    cout<<"\n--- Events with Participants ---\n";
+    Event::viewEvents();
+    cout<<"\nParticipants:\n";
+    viewParticipants();
+}
+
+void Participants::generateReport()
+{
+    cout<<"\n=== FULL REPORT ===\n";
+    Event::generateReport();
+    cout<<"\nParticipants:\n";
+    viewParticipants();
+}
